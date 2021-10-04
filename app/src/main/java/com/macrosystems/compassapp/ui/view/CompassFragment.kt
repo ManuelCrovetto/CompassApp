aecadf247ba904bf7735262c51add5af90e921bd
manuelcrovetto <Solmelia7!>
2021-10-04 23:33:44 +0200

    Cleaning code

--- app/src/main/java/com/macrosystems/compassapp/ui/view/CompassFragment.kt
+++ app/src/main/java/com/macrosystems/compassapp/ui/view/CompassFragment.kt
@@ -2,9 +2,7 @@ package com.macrosystems.compassapp.ui.view
 
 import android.Manifest
 import android.app.Activity.RESULT_OK
-
 import android.content.pm.PackageManager
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -25,10 +23,11 @@ import com.google.android.libraries.places.api.model.Place
 import com.google.android.libraries.places.widget.Autocomplete
 import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
 import com.macrosystems.compassapp.R
-import com.macrosystems.compassapp.core.utils.DialogFragmentLauncher
 import com.macrosystems.compassapp.core.ex.longToast
 import com.macrosystems.compassapp.core.ex.shortToast
 import com.macrosystems.compassapp.core.ex.showDialog
+import com.macrosystems.compassapp.core.utils.DialogFragmentLauncher
+import com.macrosystems.compassapp.data.model.CompassData
 import com.macrosystems.compassapp.data.model.NavigationDetails
 import com.macrosystems.compassapp.databinding.CompassFragmentBinding
 import com.macrosystems.compassapp.ui.view.dialogs.ErrorDialog
@@ -36,9 +35,6 @@ import com.macrosystems.compassapp.ui.viewmodel.CompassFragmentViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 import javax.inject.Inject
-import kotlin.math.atan2
-import kotlin.math.cos
-import kotlin.math.sin
 
 @AndroidEntryPoint
 class CompassFragment: Fragment(){
@@ -99,6 +95,69 @@ class CompassFragment: Fragment(){
 
     }
 
+    private fun enableLocation() {
+        if (isLocationPermissionGranted()){
+            viewModel.startLocationUpdates()
+        } else {
+            requestLocationPermission()
+        }
+    }
+
+    private fun initObservers() {
+        with(viewModel){
+            userLocation.observe(viewLifecycleOwner, {
+                actualLatLng = it
+                if (::destinationLatLng.isInitialized) {
+                    if (animationLogicCounter == 0) {
+                        YoYo.with(Techniques.SlideInDown).duration(1000).playOn(binding.tvDistanceFromDestination)
+                        binding.tvDistanceFromDestination.isVisible = true
+                        calculateDistance(destinationLatLng, false)
+                        binding.tvDistanceFromDestination.text = requireContext().getString(R.string.distance_from_destination, distanceToDestination.value ?: "loading...")
+                        animationLogicCounter++
+                    } else {
+                        binding.tvDistanceFromDestination.isVisible = true
+                        calculateDistance(destinationLatLng, false)
+                        binding.tvDistanceFromDestination.text = requireContext().getString(R.string.distance_from_destination, distanceToDestination.value.toString())
+                    }
+
+                } else {
+                    YoYo.with(Techniques.FadeOut).duration(500).playOn(binding.tvDistanceFromDestination)
+                    binding.tvDistanceFromDestination.isGone = true
+                }
+            })
+
+            savedNavigationDetails.observe(viewLifecycleOwner, {
+                actualLatLng = it.actualLatLng
+                destinationLatLng = it.destinationLatLng
+                destinationAddress = it.destinationAddress
+                binding.tvActualSelectedDestination.text = getString(R.string.actual_destination, it.destinationAddress)
+            })
+
+            compassData.observe(viewLifecycleOwner, {
+                binding.tvDegrees.text = requireContext().getString(R.string.degrees_textview, it.azimuth.toString(), it.cardinalPoint)
+                binding.ivCompass.rotation = (-it.azimuth!!).toFloat()
+
+                setDestinationBearingArrow(it)
+            })
+
+            lifecycleScope.launchWhenStarted {
+                viewState.collect { viewState ->
+                    updateUI(viewState)
+                }
+            }
+
+        }
+    }
+
+    private fun setDestinationBearingArrow(compassData: CompassData) {
+        if (::actualLatLng.isInitialized && ::destinationLatLng.isInitialized){
+            binding.ivDestinationDirection.isVisible = true
+            getDestinationBearing = compassData.azimuth!! - viewModel.calculateBearing(destinationLatLng)
+            binding.ivDestinationDirection.rotation = (getDestinationBearing.toFloat())
+        }
+    }
+
+
     private fun setUpListeners() {
         binding.btnSetNewDestination.setOnClickListener {
             val fields = listOf(Place.Field.ADDRESS, Place.Field.LAT_LNG, Place.Field.NAME)
@@ -111,58 +170,6 @@ class CompassFragment: Fragment(){
                 val action = CompassFragmentDirections.actionCompassFragmentToMapFragment(destinationLatLng.latitude.toString(), destinationLatLng.longitude.toString(), destinationAddress)
                 findNavController().navigate(action)
             }
-        }
-    }
-
-    private fun initObservers() {
-        with(viewModel){
-            location.observe(viewLifecycleOwner, {
-                actualLatLng = it
-                if (::destinationLatLng.isInitialized) {
-                    if (animationLogicCounter == 0){
-                        YoYo.with(Techniques.SlideInDown).duration(1000).playOn(binding.tvDistanceFromDestination)
-                        binding.tvDistanceFromDestination.isVisible = true
-                        calculateDistance(destinationLatLng, false)
-                        binding.tvDistanceFromDestination.text = requireContext().getString(R.string.distance_from_destination, distance.value ?: "loading...")
-                        animationLogicCounter++
-                    } else {
-                        binding.tvDistanceFromDestination.isVisible = true
-                        calculateDistance(destinationLatLng, false)
-                        binding.tvDistanceFromDestination.text = requireContext().getString(R.string.distance_from_destination, distance.value.toString())
-                    }
-
-                } else {
-                    YoYo.with(Techniques.FadeOut).duration(500).playOn(binding.tvDistanceFromDestination)
-                    binding.tvDistanceFromDestination.isGone = true
-                }
-            })
-
-
-            savedNavigationDetails.observe(viewLifecycleOwner, {
-                actualLatLng = it.actualLatLng
-                destinationLatLng = it.destinationLatLng
-                destinationAddress = it.destinationAddress
-                binding.tvActualSelectedDestination.text = getString(R.string.actual_destination, it.destinationAddress)
-
-            })
-
-            compassData.observe(viewLifecycleOwner, {
-                binding.tvDegrees.text = requireContext().getString(R.string.degrees_textview, it.azimuth.toString(), it.cardinalPoint)
-                binding.ivCompass.rotation = (-it.azimuth!!).toFloat()
-
-                if (::actualLatLng.isInitialized && ::destinationLatLng.isInitialized){
-                    binding.ivDestinationDirection.isVisible = true
-                    getDestinationBearing = it.azimuth - calculateBearing()
-                    binding.ivDestinationDirection.rotation = (getDestinationBearing.toFloat())
-                }
-            })
-
-            lifecycleScope.launchWhenStarted {
-                viewState.collect{ viewState ->
-                    updateUI(viewState)
-                }
-            }
-
         }
     }
 
@@ -224,13 +231,7 @@ class CompassFragment: Fragment(){
     //Permission Logics
     private fun isLocationPermissionGranted() = ContextCompat.checkSelfPermission((requireActivity()), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(requireActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
 
-    private fun enableLocation() {
-        if (isLocationPermissionGranted()){
-            viewModel.startLocationUpdates()
-        } else {
-            requestLocationPermission()
-        }
-    }
+
 
     private fun requestLocationPermission() {
         if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), Manifest.permission.ACCESS_FINE_LOCATION)){
@@ -255,17 +256,6 @@ class CompassFragment: Fragment(){
         viewModel.stopLocationUpdates()
         viewModel.stopCompass()
         saveNavigationOnPersistence()
-    }
-    //
-
-    private fun calculateBearing(): Double{
-        val latitude1 = Math.toRadians(actualLatLng.latitude)
-        val latitude2 = Math.toRadians(destinationLatLng.latitude)
-        val longDiff = Math.toRadians(destinationLatLng.longitude - actualLatLng.longitude)
-        val y = sin(longDiff) * cos(latitude2)
-        val x = cos(latitude1) * sin(latitude2) - sin(latitude1) * cos(latitude2) * cos(longDiff)
-
-        return (Math.toDegrees(atan2(y, x)) + 360) % 360
     }
 
     private fun saveNavigationOnPersistence(){
